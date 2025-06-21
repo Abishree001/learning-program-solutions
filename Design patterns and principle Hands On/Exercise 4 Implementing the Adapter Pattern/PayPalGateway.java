4f505d8e5a51a6823c21bfa48d1e9c571cb29750
Abishree Murugesan ✨ <149048736+Abishree001@users.noreply.github.com>
2025-06-21 18:23:04 +0530

    Add files via upload

---
+++ Design patterns and principle Hands On/Exercise 4 Implementing the Adapter Pattern/PayPalGateway.java
@@ -0,0 +1,5 @@
+public class PayPalGateway {
+    public void sendPayment(double amountInDollars) {
+        System.out.println(" Payment of $" + amountInDollars + " processed via PayPal.");
+    }
+}
---
+++ Design patterns and principle Hands On/Exercise 4 Implementing the Adapter Pattern/StripeGateway.java
@@ -0,0 +1,5 @@
+public class StripeGateway {
+    public void makeStripePayment(double amount) {
+        System.out.println("Payment of $" + amount + " processed via Stripe.");
+    }
+}
