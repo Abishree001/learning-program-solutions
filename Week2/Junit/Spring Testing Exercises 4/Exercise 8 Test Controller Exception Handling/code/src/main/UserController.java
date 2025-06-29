fc5dbc3bcc50cc577525206b1305149d494df297
Abishree Murugesan <149048736+Abishree001@users.noreply.github.com>
2025-06-29 17:37:49 +0530

    Add files via upload

---
+++ Week2/Junit/Spring Testing Exercises 4/Exercise 8 Test Controller Exception Handling/code/src/main/UserController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable int id) {
+        throw new NoSuchElementException("User not found");
+    }
+}
