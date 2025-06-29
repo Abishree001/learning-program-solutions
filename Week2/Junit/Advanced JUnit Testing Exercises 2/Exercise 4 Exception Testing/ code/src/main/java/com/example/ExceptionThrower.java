99930c72618e57715c08ef870364dd74d9fc2405
Abishree Murugesan <149048736+Abishree001@users.noreply.github.com>
2025-06-29 15:44:33 +0530

    Add files via upload

---
+++ Week2/Junit/Advanced JUnit Testing Exercises 2/Exercise 4 Exception Testing/ code/src/main/java/com/example/ExceptionThrower.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class ExceptionThrower {
+
+    public void throwException() throws IllegalArgumentException {
+        throw new IllegalArgumentException("This is an illegal argument exception");
+    }
+}
